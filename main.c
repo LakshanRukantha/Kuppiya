2cb08d15d6787f5b1ec6866d702ffe69f59a426f
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2022-11-16 15:55:59 +0530

    first commit

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //This is a comment.
+
+    /* This
+    is
+    a
+    multiline
+    comment
+    */
+
+    //Data types
+
+    int num =10;
+    char letter ='A';
+    char name[20] = "Lakshan Rukantha";
+    float num2 = 10.52;
+    double num3 = 10.5555555555555;
+
+    printf("%d\n", num);
+    printf("%c\n", letter);
+    printf("%s\n", name);
+    printf("%f\n", num2);
+    printf("%f\n", num3);
+
+
+    return 0;
+}
